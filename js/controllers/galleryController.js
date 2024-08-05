e6a76a6c213e3ad01ff47eb51f0ca4584b646250
Shlomit Horn <shlomithorn@gmail.com>
2024-08-05 19:34:03 +0300

    Add gallery grid, funcs renderGallery, onImgSelect

--- js/controllers/galleryController.js
+++ js/controllers/galleryController.js
@@ -1,5 +1,19 @@
-'use strict'
+"use strict"
 
 function renderGallery() {
     const gallery = getGallery()
+    const elGallery = document.querySelector(".memes-gallery-grid")
+    let strHtml = ""
+
+    strHtml += gallery.map((img) => `<img src="${img.url}" 
+    onclick="onImgSelect(${img.id})">`).join("")
+
+    elGallery.innerHTML = strHtml
+}
+
+function onImgSelect(id) {
+    const img = getImgById(id)
+    setMemeImg(img)
+    renderMeme()
+    openEditor()
 }
